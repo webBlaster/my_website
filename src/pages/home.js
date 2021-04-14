9cd99256cf035ea608a3e072be2440c6588bf582
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-04-14 01:46:16 +0100

    initial design

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="intro">
+        <h1>Ajibola Yekinni</h1>
+        <p>
+          Software Engineer
+          <br /> building Products with JavaScript and Python
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
